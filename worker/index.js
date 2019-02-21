964ed132849806dc48c9e6c96dfc34c29dbb3e6f
David Salgado <david@digitalronin.com>
2019-02-21 18:32:18 +0000

    Fix eslint warnings

--- worker/index.js
+++ worker/index.js
@@ -4,7 +4,7 @@ const redis = require('redis');
 const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
-  retry_strategy: () => 1000
+  retry_strategy: () => 1000,
 });
 
 const sub = redisClient.duplicate();
@@ -12,9 +12,9 @@ const sub = redisClient.duplicate();
 function fib(index) {
   if (index < 2) {
     return 1;
-  } else {
-    return fib(index - 1) + fib(index -2);
   }
+
+  return fib(index - 1) + fib(index - 2);
 }
 
 sub.on('message', (channel, message) => {
