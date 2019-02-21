04295eb65527b3378c6fc748eb961c85d01d45b1
David Salgado <david@digitalronin.com>
2019-02-21 18:29:41 +0000

    Remove the react logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
@@ -11,7 +10,6 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
